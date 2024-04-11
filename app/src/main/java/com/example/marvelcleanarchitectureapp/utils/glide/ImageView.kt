6150b4adce0634339f7bb4e28b39e335c783e66d
Antonio Rosete <mrosete-a@hotmail.com>
2024-04-11 09:40:48 -0600

    changes

--- app/src/main/java/com/example/marvelcleanarchitectureapp/utils/glide/ImageView.kt
+++ app/src/main/java/com/example/marvelcleanarchitectureapp/utils/glide/ImageView.kt
@@ -4,10 +4,11 @@ import android.util.Log
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 
 @BindingAdapter("imageUrl")
 fun loadImage(view: ImageView, url: String) {
-    Log.d("DEBUGIMAGE", url)
-    Glide.with(view.context).load(url).into(view)
+    Log.d("DEBUGIMG", url)
+    Glide.with(view.context).load(url).apply(RequestOptions.overrideOf(300, 200)).into(view)
 }
